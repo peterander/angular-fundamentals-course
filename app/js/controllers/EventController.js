6a2290a7a39859152bc417927707eced9a1d9c21
peterander <paanderson1@gmail.com>
2014-11-20 17:10:03 -0800

    added ngSanitize dependency

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -3,6 +3,7 @@
 eventsApp.controller('EventController', 
 	function EventsController($scope) {
 
+		$scope.snippet = '<span style="color:red">hi there</span>';
 		$scope.event = {
 			name: 'Angular Boot Camp',
 			date: '1/1/2013',
