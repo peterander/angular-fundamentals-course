308c690e23ccc5f5306fc4604bdc66d1ea6db876
peterander <paanderson1@gmail.com>
2014-11-24 13:03:23 -0800

    finished controllers and markup lesson

--- DemoApp/app/js/controllers/EventController.js
+++ DemoApp/app/js/controllers/EventController.js
@@ -23,7 +23,7 @@ eventsApp.controller('EventController',
 				{
 					name: 'Directives Masterclass',
 					creatorName: 'Bob Smith',
-					duration: '1 hour',
+					duration: 1,
 					level: 'Advanced',
 					abstract: 'In this session you will learn the ins and outs of directives!',
 					upVoteCount: 0
@@ -31,7 +31,7 @@ eventsApp.controller('EventController',
 				{
 					name: 'Scopes for fun and profit',
 					creatorName: 'John Doe',
-					duration: '30 mins',
+					duration: 2,
 					level: 'Introductory',
 					abstract: 'This session will take a closer look at scopes. Learn what they do, how they do it, and how to get them to do it for you.',
 					upVoteCount: 0
@@ -39,7 +39,7 @@ eventsApp.controller('EventController',
 				{
 					name: 'Well Behaved Controllers',
 					creatorName: 'Jane Doe',
-					duration: '2 hours',
+					duration: 4,
 					level: 'Intermediate',
 					abstract: 'Controllers are the beginning of everything Angular does. Learn how to craft controllers that will win the respect of your friends and neighbors.',
 					upVoteCount: 0
